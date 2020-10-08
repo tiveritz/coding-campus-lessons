d8f2563b838a3abfe3b7978531dd3466fcceb936
tiveritz <lukas.walch@icloud.com>
2020-10-08 21:04:21 +0200

    Add methods

--- src/com/dcv/oct/Day11Part02.java
+++ src/com/dcv/oct/Day11Part02.java
@@ -10,11 +10,22 @@ public class Day11Part02 {
         // equals()
         // concat()
         // contains()
-        // toLowerCase()
-        // toUpperCase()
         // startsWith()
         // endsWith()
-        // 
+        // toLowerCase()
+        // toUpperCase()
+        // indexOf()
+        // lastIndexOf()
+        // isBlank()
+        // isEmpty()
+        // length()
+        // replaceAll()
+        // matches()
+
+        // split()
+
+
+        System.out.println("helloWorld.charAt(1): " + helloWorld.charAt(1));
 
         System.out.println("helloWorld.compareTo(\"Apfelsaft\"): " + helloWorld.compareTo("Apfelsaft"));
         System.out.println("helloWorld.compareTo(\"Zugführer\"): " + helloWorld.compareTo("Zugführer"));
@@ -36,11 +47,65 @@ public class Day11Part02 {
         
         System.out.println("helloWorld.endsWith(\"Hell\"): " + helloWorld.endsWith("Hell"));
         System.out.println("helloWorld.endsWith(\"ld\"): " + helloWorld.endsWith("ld"));
-
+        
         System.out.println("helloWorld.toLowerCase(\"Worl\"): " + helloWorld.toLowerCase());
         System.out.println("helloWorld.toUpperCase(\"Worl\"): " + helloWorld.toUpperCase());
         
-        System.out.printf("%5d%n", 100);
-        System.out.printf("%05d%n", 100);
+        // System.out.printf("%5d%n", 100);
+        // System.out.printf("%05d%n", 100);
+
+        String myFormattedString = String.format("%5d%n", 100);
+        System.out.print(myFormattedString);
+        
+        System.out.println("helloWorld.indexOf(\"e\"): " + helloWorld.indexOf("e"));
+        System.out.println("helloWorld.indexOf(\"o\"): " + helloWorld.indexOf("o"));
+        System.out.println("helloWorld.indexOf(\"o\", 6): " + helloWorld.indexOf("o", 6));
+        
+        System.out.println("helloWorld.lastIndexOf(\"o\"): " + helloWorld.lastIndexOf("o"));
+        
+        String blanks = "                  ";
+        System.out.println("\"" + blanks + "\".isBlank(): " + blanks.isBlank());
+        System.out.println("\"" + blanks + "\".isEmpty(): " + blanks.isEmpty());
+        System.out.println("\"\".isEmpty(): " + "".isEmpty());
+        
+        System.out.println("helloWorld.length(): " + helloWorld.length());
+
+        System.out.println("helloWorld.repeat(3): " + helloWorld.repeat(3));
+
+        System.out.println(helloWorld.replaceAll("Hello", "Good night"));
+        System.out.println(helloWorld.replaceAll(" World", ""));
+        
+        String textForReplace = "The resulting     pattern can then be used to create";
+        System.out.println(textForReplace);
+        System.out.println(textForReplace.replaceAll(" ", "-"));
+        System.out.println(textForReplace.replaceAll("[ ]+", " "));
+        System.out.println(textForReplace.replaceAll("[AEIOUaeiou]", "#"));
+        System.out.println(textForReplace.replaceAll("[a-z]", "#"));
+        
+        String deutscheSprache = "ÄÖÜäöüß";
+        System.out.println(deutscheSprache + " -> " + deutscheSprache
+                .replaceAll("[Ä]", "Ae")
+                .replaceAll("[Ö]", "Oe")
+                .replaceAll("[Ü]", "Ue")
+                .replaceAll("[ä]", "ae")
+                .replaceAll("[ö]", "oe")
+                .replaceAll("[ü]", "ue")
+                .replaceAll("[ß]", "ss"));
+        
+        String testPLZ1 = "1234";
+        String testPLZ2 = "234";
+        String testPLZ3 = "11234";
+        String testPLZ4 = "1aaa";
+
+        System.out.println(testPLZ1 + " is a valid postal code? " + testPLZ1.matches("[1-9][0-9]{3}"));
+        System.out.println(testPLZ2 + " is a valid postal code? " + testPLZ2.matches("[1-9][0-9]{3}"));
+        System.out.println(testPLZ3 + " is a valid postal code? " + testPLZ3.matches("[1-9][0-9]{3}"));
+        System.out.println(testPLZ4 + " is a valid postal code? " + testPLZ4.matches("[1-9][0-9]{3}"));
+    
+        String[] words = helloWorld.split(" ");
+        System.out.println("Split auf Words:");
+        for (String oneWord : words) {
+            System.out.println(oneWord);
+        }
     }
 }
