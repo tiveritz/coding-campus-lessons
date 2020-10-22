066e071545a28d2c3544c04d09b7784dc9b8b531
tiveritz <lukas.walch@icloud.com>
2020-10-22 18:10:56 +0200

    Add code

--- src/com/dcv/oct/Day17Part01.java
+++ src/com/dcv/oct/Day17Part01.java
@@ -3,11 +3,14 @@ package src.com.dcv.oct;
 import java.util.Random;
 
 public class Day17Part01 {
-    private static Random random = new Random();
+    // Seed = numbers are random, but every execution returns the same numbers
+    private static Random random = new Random(3024);
 
     public static void randomRepetition() {
+        String[] words = {"Audi", "BMW", "Mercedes", "Seat", "Volkswagen"};
+
         for (int i = 0; i < 10; i++) {
-            System.out.println(random.nextInt(10));
-        }    
+            System.out.println(words[random.nextInt(words.length)]);
+        }   
     }
 }
