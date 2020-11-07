53caa5d7c4dd1543552d81f03d09c5a0ecf7b903
tiveritz <lukas.walch@icloud.com>
2020-11-07 10:27:29 +0100

    Comment

--- src/com/dcv/nov/Day20Part01.java
+++ src/com/dcv/nov/Day20Part01.java
@@ -47,6 +47,11 @@ public class Day20Part01 {
         printCanvas(getCircleToCanvas(canvas, radius));
     }
 
+    /** Fills a passed two dimensional aray with a given character.
+      * Overwrites the passed array. 
+      * @param arr The array to be filled
+      * @param character The caracter the array is filled with
+      */
     private static void fillCanvas(char[][] arr, char character) {
         for (int row = 0; row < arr.length; row++) {
             for (int col = 0; col < arr[row].length; col++) {
@@ -55,27 +60,56 @@ public class Day20Part01 {
         }
     }
 
+    /** Prints the row - inversed array into the console.
+      * @param arr Two dimensional array
+      */
     private static void printCanvas(char[][] arr) {
-        System.out.println("y");
-        System.out.println("↑");
-        for (int row = arr.length-1; row >= 0; row--) {
-            System.out.print('|');
-            for (int col = 0; col < arr[row].length; col++) {
-                System.out.print(" " + arr[row][col]);
+          System.out.println("y");
+          System.out.println("↑");
+          for (int row = arr.length-1; row >= 0; row--) {
+              System.out.print('|');
+              for (int col = 0; col < arr[row].length; col++) {
+                  System.out.print(" " + arr[row][col]);
+                }
+                System.out.println();
             }
-            System.out.println();
+            System.out.println(" " + "-".repeat(arr[0].length * 2) + "→ x" + "\n\n");
         }
-        System.out.println(" " + "-".repeat(arr[0].length * 2) + "→ x" + "\n\n");
+    
+    /** Returns the duplicated two dimensional array
+      * @param oldArr Two dimensional array
+      * @return The duplicated array
+      */
+    private static char[][] copyCanvas(char[][] oldArr) {
+        char[][] newArr = new char[oldArr.length][oldArr[0].length];
+        
+        for(int i = 0; i < oldArr.length; i++) {
+            for(int j = 0; j < oldArr[i].length; j++) {
+                newArr[i][j] = oldArr[i][j];
+            }
+        }
+        return newArr;
     }
 
+    /** Calculates the x and y center of the array and passes them with the
+      * original arguments to the overloaded method. 
+      * @param arr Two dimensional char[][]
+      * @param length Side length of the square
+      */
     private static char[][] getSquareToCanvas(char[][] arr, int length) {
         int startRow = arr.length / 2 - length / 2;
         int startCol = arr[0].length / 2 - length / 2;
         return getSquareToCanvas(arr, startRow, startCol, length);
     }
 
+    /** Returns a new two dimensional array with a square
+      * @param arr A two dimensional char[][]
+      * @param xMin The x starting coordinate of the square
+      * @param yMin The y starting coordinate of the square
+      * @param length Side length of the square
+      * @return A copy of the passed array with the square
+      */
     private static char[][] getSquareToCanvas(char[][] arr, int xMin, int yMin, int length) {
-
         char[][] square = copyCanvas(arr);
 
         int colMaxIndex = xMin + length;
@@ -94,22 +128,24 @@ public class Day20Part01 {
         }
         return square;
     }
-    
-        private static char[][] copyCanvas(char[][] oldArr) {
-            char[][] newArr = new char[oldArr.length][oldArr[0].length];
-            
-            for(int i = 0; i < oldArr.length; i++) {
-                for(int j = 0; j < oldArr[i].length; j++) {
-                    newArr[i][j] = oldArr[i][j];
-                }
-            }
-            return newArr;
-        }
 
+    /** Passes the same paramters to the overloaded methods along with the 
+      * copyArray boolean set to true 
+      */
     private static char[][] getLineToCanvas(char[][] arr, int xMin, int yMin, int xMax, int yMax) {
         return(getLineToCanvas(arr, xMin, yMin, xMax, yMax, true));
     }
 
+    /** Returns a two dimensional array with the line
+      * @param arr Two dimensional array
+      * @param xMin Starting x coordinate of Point 1
+      * @param yMin Starting y coordinate of Point 1
+      * @param xMax Starting x coordinate of Point 2
+      * @param yMax Starting y coordinate of Point 2
+      * @param copyArray Boolean that determines wheter to change original array
+                         or use a duplicate
+      * @return Duplicated or original array with line
+      */
     private static char[][] getLineToCanvas(char[][] arr, int xMin, int yMin, int xMax, int yMax, boolean copyArray) {
         /*
          *      y
@@ -156,12 +192,24 @@ public class Day20Part01 {
         return line;
     }
 
+    /** Calculates the x and y center of the array and passes them with the
+      * original arguments to the overloaded method. 
+     * @param arr Two dimensional char[][]
+     * @param length Side length of the triangle
+     */
     private static char[][] getTriangleToCanvas(char[][] arr, int length) {
         int xCenter = arr.length / 2;
         int yCenter = arr[0].length / 2;
         return getTriangleToCanvas(arr, xCenter, yCenter, length);
     }
 
+    /** Returns a two dimensional with the triangle
+      * @param arr Two dimensional char[][]
+      * @param xCenter Center x coordinate of the triangle
+      * @param yCenter Center y coordinate of the triangle
+      * @param length Side length of the triangle
+      * @return Duplicate of the array with the triangle
+      */
     private static char[][] getTriangleToCanvas(char[][] arr, int xCenter, int yCenter, int length) {
         char[][] triangle = copyCanvas(arr);
 
@@ -200,12 +248,24 @@ public class Day20Part01 {
         return triangle;
     }
 
+    /** Calculates the x and y center of the array and passes them with the
+    * original arguments to the overloaded method. 
+     * @param arr Two dimensional char[][]
+     * @param r Radius of the circle
+     */
     private static char[][] getCircleToCanvas(char[][] arr, int r) {
         int xCenter = arr.length / 2;
         int yCenter = arr[0].length / 2;
         return getCircleToCanvas(arr, xCenter, yCenter, r);
     }
 
+    /** Returns a two dimensional array with the circle
+      * @param arr Two dimensional char[][]
+      * @param xCenter Center x coordinate of the circle
+      * @param yCenter Center y coordinate of the circle
+      * @param r Radius of the circle
+      * @return Duplicate of the array with the circle
+      */
     public static char[][] getCircleToCanvas(char[][] arr, int xCenter, int yCenter, int r) {
         /*
          *   y
@@ -220,7 +280,7 @@ public class Day20Part01 {
          * b... yCenterTriangle
          * r^2 = (x-a)^2 + (y-b)^2
          * 
-         * Actually the difficulty here is to consider that the square root
+         * The difficulty here is to consider that the square root
          * in the circle formular returns + and -
          */
 
