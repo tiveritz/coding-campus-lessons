28eec9acf92b8fe7cf374562eb3c528ae5da3588
tiveritz <lukas.walch@icloud.com>
2020-11-26 18:22:07 +0100

    Add Date

--- src/com/dcv/nov/day29part01/Human.java
+++ src/com/dcv/nov/day29part01/Human.java
@@ -1,13 +1,34 @@
 package src.com.dcv.nov.day29part01;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+
 public class Human {
 	private String firstName;
 	private String lastName;
 	private String dateOfBirth;
 	private String placeOfBirth;
 	private String gender;
-
+	private Date date;
+	private int day;
+	private int month;
+	private int year;
+	
 	Human(String firstName, String lastName, String dateOfBirth, String placeOfBirth, String gender) {
+		try {
+			this.date = new SimpleDateFormat("dd.MM.yyyy").parse(dateOfBirth);
+		} catch (Exception e) {
+			System.out.println("badly handled exception");
+		}
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(date);
+		this.day = calendar.get(Calendar.DAY_OF_MONTH);
+		this.month = calendar.get(Calendar.MONTH);
+		this.year = calendar.get(Calendar.YEAR);
+
+		
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.dateOfBirth = dateOfBirth;
@@ -33,5 +54,9 @@ public class Human {
 
 	public String getGender() {
 		return gender;
+	}
+
+	public int getBirthDay() {
+		return day;
 	}
 }
