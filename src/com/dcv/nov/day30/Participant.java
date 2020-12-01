f73bdc0699aff07eece655766524eccee844de70
tiveritz <lukas.walch@icloud.com>
2020-12-01 22:58:32 +0100

    Add type to Person child classes

--- src/com/dcv/nov/day30/Participant.java
+++ src/com/dcv/nov/day30/Participant.java
@@ -2,6 +2,6 @@ package src.com.dcv.nov.day30;
 
 public class Participant extends Person {
 	Participant(String firstName, String lastName, Address address) {
-		super(firstName, lastName, address);
+		super(firstName, lastName, address, "Participant");
 	}
 }
--- src/com/dcv/nov/day30/Trainer.java
+++ src/com/dcv/nov/day30/Trainer.java
@@ -2,6 +2,6 @@ package src.com.dcv.nov.day30;
 
 public final class Trainer extends Person {
 	Trainer(String firstName, String lastName, Address address) {
-		super(firstName, lastName, address);
+		super(firstName, lastName, address, "Trainer");
 	}
 }
