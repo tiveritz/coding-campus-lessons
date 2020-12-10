613f8630977a73f0e6ea2a7c448ba7e9763326af
tiveritz <lukas.walch@icloud.com>
2020-12-10 21:26:45 +0100

    Refactor

---
+++ src/com/dcv/nov/day34version1/Enclosure.java
@@ -0,0 +1,43 @@
+package src.com.dcv.nov.day34version1;
+
+import java.util.Vector;
+
+public class Enclosure {
+	private String fence;
+	private Vector<Animal> animals;
+
+	public Enclosure(Zoo zoo, String fence) {
+		this.fence = fence;
+		zoo.addEnclosure(this);
+	}
+	
+	public String getStructure() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("  L ")
+		  .append("Enclosure with ")
+		  .append(fence)
+		  .append("\n");
+		
+		if (animals != null) {
+			for (Animal animal : animals) {
+				sb.append("      L ").append(animal.toString()).append("\n");
+			}
+		}
+		return sb.toString();
+	}
+
+	public int getNumberOfAnimals() {
+		if (animals != null) {
+			return animals.size();
+		} else {
+			return 0;
+		}
+	}
+
+	public void addAnimal(Animal a) {
+		if (animals == null) {
+			this.animals = new Vector<>();
+		}
+		animals.add(a);
+	}
+}
