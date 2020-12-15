8a33fd9b4118c1ad230e49a8f9ccd040b356d274
tiveritz <lukas.walch@icloud.com>
2020-12-15 20:10:22 +0100

    Add functinality for personnel

---
+++ src/com/dcv/dec/day35/Palaeontologist.java
@@ -0,0 +1,17 @@
+package src.com.dcv.dec.day35;
+
+public class Palaeontologist {
+	/* someone who studies fossils as a way of getting information about the
+	 * history of life on Earth.
+	 */
+	
+	 private String name;
+
+	 protected Palaeontologist(String name) {
+		 this.name = name;
+	 }
+
+	 public String getName() {
+		 return name;
+	 }
+}
