aa1739eae58bdacf88d5e6b2b81ddfa27315e531
tiveritz <lukas.walch@icloud.com>
2021-01-14 17:07:36 +0100

    Preparation for search algorithms

---
+++ src/com/dcv/jan/day38/Main.java
@@ -0,0 +1,16 @@
+package src.com.dcv.jan.day38;
+
+public class Main {
+	public static void main (String[] args) {
+		System.out.println("hello world");
+
+		String[] arr = new String[]{"Lukas", "Alex", "Ali", "Eric"};
+		
+
+		// Linear search
+
+		// Binary search
+
+		// Greedy Algorithmus
+	}
+}
